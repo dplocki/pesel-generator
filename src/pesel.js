fdc7515f14ef8e30356051b8a8bff407f34fab8e
Dawid Płocki <dawid.plocki@gmail.com>
2018-07-30 22:58:35 +0200

    Refactoring of PESEL checking function

--- src/pesel.js
+++ src/pesel.js
@@ -1,3 +1,18 @@
-export function check_pesel(pesel) {
-  return pesel.length === 11;
+export function checkPESEL(pesel) {
+  if (pesel.length !== 11) {
+    return false;
+  }
+
+  const weights = [9, 7, 3, 1];
+
+  let sum = 0;
+  for (let i = 0; i < 10; i++) {
+    sum += characterToDigit(pesel.charAt(i)) * weights[i % 4];
+  }
+
+  return sum % 10 === characterToDigit(pesel.charAt(10));
+}
+
+function characterToDigit(character) {
+  return character - '0';
 }
--- src/pesel.test.js
+++ src/pesel.test.js
@@ -1,12 +1,16 @@
-import { check_pesel } from './pesel'
-
-it('validation should show that PESEL is correct', () => {
-  expect(check_pesel('43031299214')).toEqual(true);
-  expect(check_pesel('10310155402')).toEqual(true);
-});
+import { checkPESEL } from './pesel'
 
 it('validation should reject PESEL without eleven digits', () => {
-  expect(check_pesel('')).toEqual(false);
-  expect(check_pesel('10310155')).toEqual(false);
-  expect(check_pesel('103101554011')).toEqual(false);
+  expect(checkPESEL('')).toEqual(false);
+  expect(checkPESEL('10310155')).toEqual(false);
+  expect(checkPESEL('103101554011')).toEqual(false);
+});
+
+it('validation should check the control sum', () => {
+  expect(checkPESEL('43031299215')).toEqual(false);
+});
+
+it('validation should show that PESEL is correct', () => {
+  expect(checkPESEL('18271117189')).toEqual(true);
+  expect(checkPESEL('10310155402')).toEqual(true);
 });
