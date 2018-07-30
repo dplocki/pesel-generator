90b35550dc13e7abae1fbfed574fa3055a306173
Dawid Płocki <dawid.plocki@gmail.com>
2018-07-30 22:17:27 +0200

    Developing last test

--- src/pesel.test.js
+++ src/pesel.test.js
@@ -5,7 +5,8 @@ it('validation should show that PESEL is correct', () => {
   expect(check_pesel('10310155402')).toEqual(true);
 });
 
-it('validation should show that PESEL is incorrect', () => {
+it('validation should reject PESEL without eleven digits', () => {
   expect(check_pesel('')).toEqual(false);
   expect(check_pesel('10310155')).toEqual(false);
+  expect(check_pesel('103101554011')).toEqual(false);
 });
