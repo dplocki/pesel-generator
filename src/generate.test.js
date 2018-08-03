79d877f00659c94f3f8f9556c554694c0c8ba4a1
Dawid Płocki <dplocki@future-processing.com>
2018-08-03 12:45:02 +0200

    Adding the editorconfig file

--- src/generate.test.js
+++ src/generate.test.js
@@ -7,9 +7,9 @@ it('generated PESEL should have eleven digits', () => {
 });
 
 it('generated PESEL from data should start with does number', () => {
-   expect(generatePESEL(moment("20111031")).substring(0, 6)).toBe('111031');
-   expect(generatePESEL(moment("19740112")).substring(0, 6)).toBe('740112');
-   expect(generatePESEL(moment("20130412")).substring(0, 6)).toBe('130412');
+  expect(generatePESEL(moment("20111031")).substring(0, 6)).toBe('111031');
+  expect(generatePESEL(moment("19740112")).substring(0, 6)).toBe('740112');
+  expect(generatePESEL(moment("20130412")).substring(0, 6)).toBe('130412');
 });
 
 it('generated PESEL should be valid one', () => {
