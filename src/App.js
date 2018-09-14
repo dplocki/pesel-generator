b2b9c19712491314ad768da41e96f3b9872ee3e7
Dawid Płocki <dplocki@future-processing.com>
2018-09-14 12:52:46 +0200

    Adding simple GUI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
+import moment from 'moment';
+import momentRandom from 'moment-random';
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { generatePESEL } from './generate';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      howMany: 5
+    };
+
+    this.handleGenerateButtonClick = this.handleGenerateButtonClick.bind(this);
+  }
+
+  handleGenerateButtonClick() {
+    const pesels = [...Array(this.state.howMany)]
+      .map(_ => generatePESEL(momentRandom(moment())));
+
+    this.setState(state => ({
+      ...state,
+      pesels
+    }));
+  }
+
   render() {
+    const pesels = this.state.pesels
+      ? this.state.pesels.reduce((result, next) => result + '\n' + next)
+      : '';
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <textarea value={pesels} rows="10" cols="11" />
+
+        <button onClick={this.handleGenerateButtonClick}>Generate</button>
       </div>
     );
   }
