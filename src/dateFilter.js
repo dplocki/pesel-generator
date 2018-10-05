1c08fd1a1b8deee0fc97034e787a076532ca59a7
Dawid Płocki <dplocki@future-processing.com>
2018-10-05 11:28:08 +0200

    Improvement of DateFilter to return non const date

--- src/dateFilter.js
+++ src/dateFilter.js
@@ -1,5 +1,7 @@
 import moment from 'moment';
 
-export function dateFilter() {
-  return moment('20190304');
+export function dateFilter(input) {
+  const onlyDigits = input.replace(/[^0-9]/g, '');
+
+  return moment(onlyDigits);
 }
--- src/dateFilter.test.js
+++ src/dateFilter.test.js
@@ -3,4 +3,5 @@ import { dateFilter } from './dateFilter';
 
 it ('For full date filter return it', () => {
   expect(dateFilter('2019/03/04').isSame(moment('20190304'))).toBe(true);
+  expect(dateFilter('1992/11/12').isSame(moment('19921112'))).toBe(true);
 });
