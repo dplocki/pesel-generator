27bfa2ca318e39b9b2ad54dba5a031be26f6d9a2
Dawid Płocki <dplocki@future-processing.com>
2018-10-12 14:12:23 +0200

    Red: Adding test for detecting of incorrect input data

--- src/oldYearFilter.test.js
+++ src/oldYearFilter.test.js
@@ -1,6 +1,12 @@
 import moment from 'moment';
 import { oldYearFilter } from './oldYearFilter';
 
-it('Providing old should be calculate to date', () => {
+it('Providing years old should be calculate to date', () => {
   expect(oldYearFilter('12', moment('20181012')).isSame(moment('20061012'))).toBe(true);
+});
+
+it('Providing incorrect years old should be return null', () => {
+  expect(oldYearFilter('a')).toBe(null);
+  expect(oldYearFilter('dkjnsdkjf')).toBe(null);
+  expect(oldYearFilter('1000')).toBe(null);
 });
