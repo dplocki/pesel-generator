7bffcf033cd73ff3874109f3c71abcc7e6b0ceee
Dawid Płocki <dplocki@future-processing.com>
2018-10-26 11:56:50 +0200

    Adding the random date for no-input

--- src/filterMerge.js
+++ src/filterMerge.js
@@ -1,7 +1,12 @@
+import momentRandom from 'moment-random';
 import { oldYearFilter } from './oldYearFilter';
 import { dateFilter } from './dateFilter';
 
 export function filterMerge(input) {
+    if (!input) {
+        return momentRandom();
+    }
+
     if (input.length <= 2) {
         return oldYearFilter(input);
     }
