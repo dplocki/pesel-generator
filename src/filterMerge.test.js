b5d155e98ee12488b6250c4c11c1d83e21a52e4a
Dawid Płocki <dplocki@future-processing.com>
2018-10-26 09:55:58 +0200

    Red: adding test for checking invoking date filter

--- src/filterMerge.test.js
+++ src/filterMerge.test.js
@@ -13,3 +13,11 @@ it ('User enter two digit or less, year old filter should called', () => {
   expect(oldYearFilterSpy).toHaveBeenCalled();
   expect(result).not.toBeNull();
 });
+
+it ('User enter more than characters than two, date filter should be called', () => {
+  var result = filterMerge('1983');
+
+  expect(dateFilterSpy).toHaveBeenCalled();
+  expect(oldYearFilterSpy).not.toHaveBeenCalled();
+  expect(result).not.toBeNull();
+});
