66fe4e54348f469764797c613a8776d68eeaea1b
Dawid Płocki <dplocki@future-processing.com>
2019-04-09 11:38:20 +0200

    Fixing problem with generating only one date

--- src/dateFilter.js
+++ src/dateFilter.js
@@ -4,20 +4,20 @@ import momentRandom from 'moment-random';
 export function dateFilter(input) {
   const onlyDigits = input.replace(/[^0-9]/g, '');
 
-  if (onlyDigits.length === 6) {
-    // Missing day number
-    const newInput = onlyDigits + '01';
-    const start = moment(newInput);
-    const end = start.add(1, 'months').add(-1, 'days');
-
-    return momentRandom(start, end);
-  } else if (onlyDigits.length === 4) {
+  if (onlyDigits.length === 4) {
     // Missing day and month
     const newInput = onlyDigits + '0101';
     const start = moment(newInput);
-    const end = start.add(1, 'years').add(-1, 'days');
+    const end = start.clone().add(1, 'years').add(-1, 'days');
 
-    return momentRandom(start, end);
+    return momentRandom(end, start);
+  } else if (onlyDigits.length === 6) {
+    // Missing day number
+    const newInput = onlyDigits + '01';
+    const start = moment(newInput);
+    const end = start.clone().add(1, 'months').add(-1, 'days');
+
+    return momentRandom(end, start);
   } else if (onlyDigits.length === 8) {
     return moment(onlyDigits);
   }
