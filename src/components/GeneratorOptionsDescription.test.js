58995d73c7d31351eee61c588d2b8b5e2b3e8cbc
Dawid Płocki <dawid.plocki@gmail.com>
2021-01-25 18:39:03 +0100

    First approach to testing component in Jest

--- src/components/GeneratorOptionsDescription.test.js
+++ src/components/GeneratorOptionsDescription.test.js
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GeneratorOptionsDescription from './GeneratorOptionsDescription';
 
+let container;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<GeneratorOptionsDescription />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.render(<GeneratorOptionsDescription />, container);
+  ReactDOM.unmountComponentAtNode(container);
+});
+
+it('renders default text on start', () => {
+  ReactDOM.render(<GeneratorOptionsDescription />, container);
+  const text = container.querySelector('.text-muted').textContent;
+  expect(text).toBe('Dowolny PESEL');
 });
