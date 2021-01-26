b6f5275731f837487fa1d4451a99ea224f30d8a0
Dawid Płocki <dawid.plocki@gmail.com>
2021-01-26 19:11:26 +0100

    Implement description logic

--- src/components/GeneratorOptionsDescription.js
+++ src/components/GeneratorOptionsDescription.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
+import { GenderEnum } from '../logic/genderEnum';
+
+
+function yearNumberToLabel(value) {
+  if (value === 1) {
+    return 'rok';
+  }
+
+  if ((value < 10 || value > 20) && (value % 10) < 5) {
+    return value + ' lata';
+  }
+
+  return value + ' lat';
+}
+
+
+function buildDescription(options) {
+  const genderLabel = {
+    [GenderEnum.Male]: 'mężczyzn',
+    [GenderEnum.Female]: 'kobiet',
+    [GenderEnum.Any]: 'osób'
+  }[options.gender];
+
+  let result = `Tylko PESEL-e ${genderLabel}`;
+
+  if (options.yearOrAge === null) {
+    return result;
+  }
+
+  const meaningLabel = {
+    '<': 'mniej niż',
+    '=<': 'co najwyżej',
+    '=': 'dokładnie',
+    '>=': 'co najmniej',
+    '>': 'więcej niż'
+  }[options.yearOrAgeMeaning];
+
+  return result + ' mających ' + meaningLabel + ' ' + yearNumberToLabel(options.yearOrAge)
+}
+
 
 export default function GeneratorOptionsDescription({ options }) {
-  const description = 'Dowolny PESEL';
-
   return <Form.Text className="text-muted">
-    {description}
+    {options ? buildDescription(options) : 'Dowolne PESEL-e'}
   </Form.Text>;
 }
--- src/components/GeneratorOptionsDescription.test.js
+++ src/components/GeneratorOptionsDescription.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GenderEnum } from '../logic/genderEnum';
 import GeneratorOptionsDescription from './GeneratorOptionsDescription';
 
 let container;
+
+function getDescription(options) {
+  ReactDOM.render(<GeneratorOptionsDescription options={options} />, container);
+  return container.querySelector('.text-muted').textContent;
+}
 
 beforeEach(() => {
   container = document.createElement('div');
@@ -19,8 +25,38 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(container);
 });
 
-it('renders default text on start', () => {
-  ReactDOM.render(<GeneratorOptionsDescription />, container);
-  const text = container.querySelector('.text-muted').textContent;
-  expect(text).toBe('Dowolny PESEL');
+it('renders with default test', () => {
+  expect(getDescription(null)).toBe('Dowolne PESEL-e');
+});
+
+it('render text for only females', () => {
+  expect(getDescription({
+    yearOrAgeMeaning: '=',
+    yearOrAge: null,
+    gender: GenderEnum.Female
+  })).toBe('Tylko PESEL-e kobiet');
+});
+
+it('render text for only male older than 23', () => {
+  expect(getDescription({
+    yearOrAgeMeaning: '>',
+    yearOrAge: 23,
+    gender: GenderEnum.Male
+  })).toBe('Tylko PESEL-e mężczyzn mających więcej niż 23 lata');
+});
+
+it('render text for only females younger that one year', () => {
+  expect(getDescription({
+    yearOrAgeMeaning: '<',
+    yearOrAge: 1,
+    gender: GenderEnum.Female
+  })).toBe('Tylko PESEL-e kobiet mających mniej niż rok');
+});
+
+it('render text for older or equal 34', () => {
+  expect(getDescription({
+    yearOrAgeMeaning: '>=',
+    yearOrAge: 34,
+    gender: GenderEnum.Any
+  })).toBe('Tylko PESEL-e osób mających co najmniej 34 lata');
 });
