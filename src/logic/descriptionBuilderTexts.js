b1c8f1f1d528314e4a30a20d8806589a80ac00cb
Dawid Płocki <dawid.plocki@gmail.com>
2021-03-12 19:41:25 +0100

    Fix typo in INCORRECT_YEAR_TEXT

--- src/logic/descriptionBuilderTexts.js
+++ src/logic/descriptionBuilderTexts.js
@@ -1,3 +1,3 @@
 export const DEFAULT_TEXT = 'Dowolne PESEL-e';
 export const ERROR_TEXT = 'To nie jest ani rok, ani poprawna data';
-export const INCORRECT_YEAR_TEXT = 'Dla prowadzonego roku nie istnieje PESEL';
+export const INCORRECT_YEAR_TEXT = 'Dla wprowadzonego roku nie istnieje PESEL';
