b01d6f1278a056a9dcfa4e4860e1db680f3d9d17
Ziomioslaw <dawid.plocki@gmail.com>
2023-06-13 21:25:17 +0200

    Update unit test for React 18

--- src/components/GeneratorOptions.test.js
+++ src/components/GeneratorOptions.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import GeneratorOptions from './GeneratorOptions';
-
+import { createRoot } from 'react-dom/client';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<GeneratorOptions />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+  const root = createRoot(div);
+  root.render(<GeneratorOptions />);
+
+  root.unmount();
 });
